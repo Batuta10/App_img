e041c7b01e8bc05cb264285cb3457978e4b5516d
Batuta10 <batuta2604@gmail.com>
2017-02-20 18:46:49 -0300

    Cor app, animação e comentarios

--- app/src/main/java/br/com/fiap/app_img/MainActivity.java
+++ app/src/main/java/br/com/fiap/app_img/MainActivity.java
@@ -3,31 +3,40 @@ package br.com.fiap.app_img;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Animation animation;
     private ImageView imgView;
+    //Array com as imagems
     private int[] imgs = {R.drawable.up,
                           R.drawable.dow,
                           R.drawable.right,
                           R.drawable.leaft,
-                          R.drawable.x,
-                          R.drawable.github};
-
+                          R.drawable.github,
+                          R.drawable.x};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-		
-        imgView = (ImageView) findViewById(R.id.imgMainView);
+
+        imgView = (ImageView) findViewById(R.id.imgMainView);//Mapea a view
+        animation = AnimationUtils.loadAnimation(this,R.anim.fade);//Mapea a animação
     }//OnCreate
 
     public void rolar(View v){
+        //Inicializando as Variaves e animaçoes
         int contImg = 0;
-		Random r = new Random();//Função para Rodar int Aleatorias
-		contImg = r.nextInt(5);
-		imgView.setImageResource(imgs[contImg]);//Set Image na imgview 
+        imgView.startAnimation(animation);
+        //Função para Rodar int Aleatorias
+        Random r = new Random();
+
+        //Sortea o numero aleatoria e joga a posição na variavel para cetar em baixo
+        contImg = r.nextInt(6);
+        imgView.setImageResource(imgs[contImg]);
     }//Rolar
 }//Class
